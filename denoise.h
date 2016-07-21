46f5e6f91862d35debb84f68e7370c49091d2a92
Christian Reinbacher <reinbacher@icg.tugraz.at>
2016-07-21 13:45:24 +0200

    removed commandline utility

--- denoise.h
+++ denoise.h
@@ -16,12 +16,6 @@ namespace  cuda {
     void solveTVIncrementalManifold(iu::ImageGpu_32f_C1 *u, iu::ImageGpu_32f_C1 *f, iu::ImageGpu_32f_C1 *t,
                                     float lambda, float lambda_time,
                                     int iterations, float u_min, float u_max, METHOD method);
-    void solveTVIncrementalManifoldAdaptiveLambda(iu::ImageGpu_32f_C1 *u, iu::ImageGpu_32f_C1 *f, iu::ImageGpu_32f_C1 *t,
-                                                  float curr_time, float lambda, float lambda_time,
-                                                  int iterations, float u_min, float u_max, METHOD method);
-    void solveTVIncrementalManifoldOccurenceLambda(iu::ImageGpu_32f_C1 *u, iu::ImageGpu_32f_C1 *f, iu::ImageGpu_32f_C1 *t,
-                                                   iu::ImageGpu_32u_C1 *o, float lambda, float lambda_time,
-                                                   int iterations, float u_min, float u_max, METHOD method);
    } // namespace cuda
 
 #endif
