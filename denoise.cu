afe09a233b48de5a533932cf9b8ab6b84feee7da
Christian Reinbacher <reinbacher@icg.tugraz.at>
2016-07-27 08:56:29 +0200

    added licence, submodules for cnpy and libcaer

--- denoise.cu
+++ denoise.cu
@@ -1,3 +1,19 @@
+// This file is part of dvs-reconstruction.
+//
+// Copyright (C) 2016 Christian Reinbacher <reinbacher at icg dot tugraz dot at>
+// Institute for Computer Graphics and Vision, Graz University of Technology
+// https://www.tugraz.at/institute/icg/teams/team-pock/
+//
+// dvs-reconstruction is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// dvs-reconstruction is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef DENOISE_CU
 #define DENOISE_CU
 
